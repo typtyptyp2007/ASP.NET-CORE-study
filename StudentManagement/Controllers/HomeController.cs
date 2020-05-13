17cc8f08f460636f67c9f42cde6ad1386a55be99
typ <typ@163.com>
2020-05-14 00:05:17 +0800

    model

--- StudentManagement/Controllers/HomeController.cs
+++ StudentManagement/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models;
 
 namespace StudentManagement.Controllers
 {
     public class HomeController : Controller
     {
-        public JsonResult Index()
+        private readonly IStudentRepository _studentRepository;
+
+        public HomeController(IStudentRepository studentRepository)
         {
-            return Json(new {id = "1", name = "张三"});
+            _studentRepository = studentRepository;
+        }
+
+        public string Index()
+        {
+            return _studentRepository.GetStudent(1).Name;
+            //return Json(new {id = "1", name = "张三"});
         }
     }
 }
